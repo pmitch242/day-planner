d06b37114ec36522d1606215397372f4c8fc9aef
Phill Mitchell-Matsuyama <phillicia.mitchell21@gmail.com>
2019-11-16 19:44:30 -0500

    edited so that the save button saves userinput to localStorage

---
+++ assets/js/script.js
@@ -0,0 +1,36 @@
+// ================VARIABLES================
+var infoCol = $(".info-col");
+var saveButton = $(".save-button");
+var userInput = $(".user-input");
+let data;
+
+// ================FUNCTIONS================
+
+
+// Function to use save button to save textarea value to localStorage
+saveButton.click(function(){
+    event.preventDefault();
+    // test to see button was pressed
+    console.log("I was pressed");
+
+    var a = this;
+    // console.log(a);
+    // console.log(a.previousElementSibling);
+    console.log(a.previousElementSibling.firstElementChild);
+    console.log($(a).prev());
+    // console.log($(results))
+    var results = a.previousElementSibling.firstElementChild;
+    console.log(results);
+    console.log($(results));
+    data = $(results).val().trim();
+    console.log(data);
+
+    console.log(results.id);
+    var time = results.id;
+    console.log(time);
+
+    localStorage.setItem(time, data);
+
+});
+
+
