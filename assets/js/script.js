2ad412a176c9bea249748633fca67b9f7f89870e
Phill Mitchell-Matsuyama <phillicia.mitchell21@gmail.com>
2019-11-16 22:06:54 -0500

    edited comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,67 +1,72 @@
 
 $(document).ready(function () {
     // ================VARIABLES================
-    var infoCol = $(".info-col");
     var saveButton = $(".save-button");
-    var userInput = $(".user-input");
     let data;
 
-    // ================FUNCTIONS================
+    // Set these values as soon as the page loads
+    // =========================================
     console.log("The current hour is " + moment().hour());
-    $("#exact-time").text(moment().format('MMMM Do YYYY, h:mm:ss a'));
 
+    //This prevents the from waiting one second before setting these values
+    $("#exact-time").text(moment().format('MMMM Do YYYY, h:mm:ss a'));
+    changeColor();
+    // =========================================
+
+    // ================FUNCTIONS================
+
+    //Run this function every second
     setInterval(function () {
+        // Update time
         $("#exact-time").text(moment().format('MMMM Do YYYY, h:mm:ss a'));
+        // Check to see if textarea column color should be changed
         changeColor();
     }, 1000);
 
     // Function to pull results from localStorage
-    for (var i = 9; i <= 17; i++){
+    for (var i = 9; i <= 17; i++) {
         $('#' + i).val(localStorage.getItem("hour " + i));
     }
 
     // Function to use save button to save textarea value to localStorage
     saveButton.click(function () {
         event.preventDefault();
+
         // test to see button was pressed
         console.log("I was pressed");
 
+        //retrieve user input
         var a = this;
-        // console.log(a);
-        // console.log(a.previousElementSibling);
-        console.log(a.previousElementSibling.firstElementChild);
-        console.log($(a).prev());
-        // console.log($(results))
         var results = a.previousElementSibling.firstElementChild;
-        console.log(results);
         console.log($(results));
         data = $(results).val().trim();
         console.log(data);
 
-        console.log(results.id);
+        //identify time row
         var time = results.id;
         console.log("hour " + time);
 
+        //save key and user input to localStorage
         localStorage.setItem("hour " + time, data);
 
     });
 
     //Function to change textarea color depending on hour of the day
-    function changeColor(){
+    function changeColor() {
 
-        for (i=9; i<=17; i++){
+        for (i = 9; i <= 17; i++) {
             var currentTime = moment().hour();
 
-            if(currentTime === i){
+            //Present
+            if (currentTime === i) {
                 $("." + i).attr("style", "background-color: red");
             }
 
-            else if(currentTime > i){
+            //Past
+            else if (currentTime > i) {
                 $("." + i).attr("style", "background-color: lightgray");
             }
         }
     }
-
-    changeColor();
 
 });
